649910f279ff20f201c2484974e966128244d279
jennysol <jennyssoliver@gmail.com>
2025-11-10 23:35:29 -0300

    feat: add front url to enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3001'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3001',
+      'https://ton-front-alpha.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
       'Content-Type',
